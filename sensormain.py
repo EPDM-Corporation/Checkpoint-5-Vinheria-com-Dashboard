d3a72c308b6c174995c54decd46c05cf43743854
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-04-26 23:46:56 -0300

    Update sensormain.py

--- sensormain.py
+++ sensormain.py
@@ -1,16 +1,13 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS  
 import requests
 import json
 
 app = Flask(__name__)
+CORS(app)  
 
 # CONFIGURAÇÕES
 IP_DO_SERVER = '52.237.23.203'  # Ip do server
-# LUMINOSIDADE_MAX = 77
-# TEMPERATURA_MAX = 36
-# TEMPERATURA_MIN = -3
-# UMIDADE_MAX = 83
-# UMIDADE_MIN = 8
 
 # Função para obter os dados de luminosidade a partir da API
 def obter_dados(lastN, sensorType, IP):
@@ -28,44 +25,35 @@ def obter_dados(lastN, sensorType, IP):
     else:
         print(f"Erro ao obter dados: {response.status_code}")
         return []
+
 # Funções para mandar os dados em uma lista
 @app.route('/sensor/luminosidade', methods=['GET'])
 def get_luminosidade():
     lastN = request.args.get('lastN', 10, type=int)  # Padrão para 10
     luminosity = obter_dados(lastN, "luminosity", IP_DO_SERVER)
     lum = [float(entry['attrValue']) for entry in luminosity]
-    return lum
+    return jsonify(lum)   
 
 @app.route('/sensor/temperatura', methods=['GET'])
 def get_temperatura():
     lastN = request.args.get('lastN', 10, type=int)  # Padrão para 10
     temperature = obter_dados(lastN, "temperature", IP_DO_SERVER)
     temp = [float(entry['attrValue']) for entry in temperature]
-    return temp
+    return jsonify(temp)
 
 @app.route('/sensor/umidade', methods=['GET'])
 def get_umidade():
     lastN = request.args.get('lastN', 10, type=int)  # Padrão para 10
     humidity = obter_dados(lastN, "humidity", IP_DO_SERVER)
     hum = [float(entry['attrValue']) for entry in humidity]
-    return hum
+    return jsonify(hum)
 
 @app.route('/sensor/tempo', methods=['GET'])
 def get_tempo():
     lastN = request.args.get('lastN', 10, type=int)  # Padrão para 10
     humidity = obter_dados(lastN, "humidity", IP_DO_SERVER)
     tempos = [entry['recvTime'][11:19] for entry in humidity]
-    return tempos
-
-
-# @app.route('/sensor/status', methods=['GET'])
-# def get_status():
-#     status = {"LUMINOSIDADE_MAX": LUMINOSIDADE_MAX, 
-#               "TEMPERATURA_MAX": TEMPERATURA_MAX, 
-#               "TEMPERATURA_MIN": TEMPERATURA_MIN, 
-#               "UMIDADE_MAX": UMIDADE_MAX, 
-#               "UMIDADE_MIN": UMIDADE_MIN}
-#     return jsonify(status)
+    return jsonify(tempos)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  # A API estará disponível na porta 5000
+    app.run(host='0.0.0.0', port=5000)
